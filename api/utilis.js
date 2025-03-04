670e00aba4ac689cb9eaaa5187e2fc6441b82dec
Suegathh <suzzainnegathu@gmail.com>
2025-03-04 03:44:45 +0300

    change from multerbto cloudinary

--- api/utilis.js
+++ api/utilis.js
@@ -1,13 +1,19 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 function authenticateToken(req, res, next) {
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split(" ")[1];
 
-    if (!token) return res.sendStatus(401);
+    if (!token) {
+        return res.status(401).json({ error: "Access denied. No token provided." });
+    }
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
+        if (err) {
+            console.error("JWT Verification Error:", err.message); // Log the error
+            return res.status(403).json({ error: "Invalid or expired token." });
+        }
+
         req.user = user;
         next();
     });
